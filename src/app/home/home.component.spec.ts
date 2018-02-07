93eb9d0ce184afb8ba1d3f626dca46e5eef2e7fd
arizona2014 <andy.lisac@gmail.com>
2018-02-07 20:19:38 +0100

    making home component test working

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,14 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { MatIconModule, MatTabsModule, MatCardModule } from "@angular/material";
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularFirestore, AngularFirestoreModule } from "angularfire2/firestore";
+import { AuthenticateService } from "../login/loginService/authenticate.service";
+import { AngularFireModule } from "angularfire2";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 describe('HomeComponent', () => {
+
+  let firebaseConfig = {
+      apiKey: "AIzaSyCaMhk0ZziaHn6QvAzBlCJA2vwFY0lx-iE",
+      authDomain: "photobooth-f83ed.firebaseapp.com",
+      databaseURL: "https://photobooth-f83ed.firebaseio.com",
+      projectId: "photobooth-f83ed",
+      storageBucket: "photobooth-f83ed.appspot.com",
+      messagingSenderId: "571573564921"
+  };
+
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      imports: [ RouterTestingModule, MatIconModule, MatTabsModule, MatCardModule, AngularFireModule.initializeApp(firebaseConfig), AngularFirestoreModule, BrowserAnimationsModule  ],
+      declarations: [ HomeComponent ],
+      providers: [ AngularFirestore, AuthenticateService ]
     })
     .compileComponents();
   }));
@@ -19,7 +36,8 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create home component', () => {
     expect(component).toBeTruthy();
   });
+
 });
