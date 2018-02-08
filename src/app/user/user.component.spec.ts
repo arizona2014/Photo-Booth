f89e88fb311c5d5df0d918fb80290ac15c571a5f
arizona2014 <andy.lisac@gmail.com>
2018-02-08 07:28:27 +0100

    refactored

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,13 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserComponent } from './user.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularFirestore, AngularFirestoreModule } from "angularfire2/firestore";
+import { AuthenticateService } from "../login/loginService/authenticate.service";
+import { AngularFireModule } from "angularfire2";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 describe('UserComponent', () => {
+
+
+  let firebaseConfig = {
+    apiKey: "AIzaSyCaMhk0ZziaHn6QvAzBlCJA2vwFY0lx-iE",
+    authDomain: "photobooth-f83ed.firebaseapp.com",
+    databaseURL: "https://photobooth-f83ed.firebaseio.com",
+    projectId: "photobooth-f83ed",
+    storageBucket: "photobooth-f83ed.appspot.com",
+    messagingSenderId: "571573564921"
+  };
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule, AngularFireModule.initializeApp(firebaseConfig), AngularFirestoreModule, BrowserAnimationsModule  ],
       declarations: [ UserComponent ]
     })
     .compileComponents();
@@ -19,7 +34,8 @@ describe('UserComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create user component', () => {
     expect(component).toBeTruthy();
   });
+
 });
