ec515c5ba70cf1fb10ebb1c1d155738975c4f2fb
arizona2014 <andy.lisac@gmail.com>
2018-02-08 07:27:12 +0100

    webcam in class

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,18 +2,57 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticateService } from "../login/loginService/authenticate.service";
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { WebCamComponent } from 'ack-angular-webcam';
+import { Http, Request } from '@angular/http';
+import { HttpClient } from "@angular/common/http";
+import { HttpHeaders } from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  providers: [ AuthenticateService ],
+  providers: [ AuthenticateService, Http ],
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
   data: any;
+  webcam:WebCamComponent;
+  base64: any;
 
-  constructor(private _service:AuthenticateService, private _db: AngularFirestore) { }
+  constructor(private _service:AuthenticateService, private _db: AngularFirestore, private http: HttpClient ) { }
+
+    genBase64(){
+        this.webcam.getBase64()
+            .then( base=>this.base64=base)
+    }
+
+    //get HTML5 FormData object and pretend to post to server
+    genPostData(){
+        this.webcam.captureAsFormData({fileName:'file.jpg'})
+            .then( formData=>this.postFormData(formData).subscribe( res => {
+                console.log(res);
+            } ))
+    }
+
+    //a pretend process that would post the webcam photo taken
+    postFormData(formData){
+        const config = {
+            url:"http://www.aviorsciences.com/",
+            body: formData
+        }
+
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type':  'image/jpg'
+            })
+        };
+
+        return this.http.post( config.url, config.body, httpOptions );
+    }
+
+    onCamError(err){}
+
+    onCamSuccess(){}
 
   ngOnInit() {
     this._service.checkCredentials();
