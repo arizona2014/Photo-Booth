6f44729176e034fb1decb9ead5a34ec140ac29fb
arizona2014 <andy.lisac@gmail.com>
2018-02-09 22:18:53 +0100

    adding firebase config to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+        apiKey: "AIzaSyCaMhk0ZziaHn6QvAzBlCJA2vwFY0lx-iE",
+        authDomain: "photobooth-f83ed.firebaseapp.com",
+        databaseURL: "https://photobooth-f83ed.firebaseio.com",
+        projectId: "photobooth-f83ed",
+        storageBucket: "photobooth-f83ed.appspot.com",
+        messagingSenderId: "571573564921"
+    }
 };
