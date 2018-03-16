1df3a2802ed689d5b94af0c0ed3c4e868f1a0619
arizona2014 <andy.lisac@gmail.com>
2018-03-16 07:04:59 +0100

    auth guard service

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,16 @@
+
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { AuthenticateService } from './login/loginService/authenticate.service';
+
+@Injectable()
+export class AuthGuardService implements CanActivate {
+    constructor(public auth: AuthenticateService, public router: Router) {}
+    canActivate(): boolean {
+        if (this.auth.isAuthenticated()) {
+            this.router.navigate(['login']);
+            return false;
+        }
+        return true;
+    }
+}
