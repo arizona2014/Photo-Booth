55f88bf79ae2dca6fcd0d961b608e2d77e0bfdb7
arizona2014 <andy.lisac@gmail.com>
2018-03-16 07:04:19 +0100

    route guard in routes files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { LoginComponent } from "./login/login.component";
 import { HomeComponent } from "./home/home.component";
+import {
+    AuthGuardService as AuthGuard
+} from './auth-guard.service';
 
 const routes: Routes = [
     { path:'', component: LoginComponent },
     { path:'login', component: LoginComponent },
-    { path:'home', component: HomeComponent },
+    { path:'home', component: HomeComponent, canActivate: [AuthGuard]  },
     { path:'**', component: LoginComponent }
 ];
 
