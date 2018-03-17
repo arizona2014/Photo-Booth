6190327694ed21fa1f7ebc1df927234aeeaca19d
arizona2014 <andy.lisac@gmail.com>
2018-03-17 20:06:20 +0100

    presenting auth guard

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,11 @@ import { AuthenticateService } from "../login/loginService/authenticate.service"
 import { AngularFirestore } from 'angularfire2/firestore';
 import { WebCamComponent } from 'ack-angular-webcam';
 import { AngularFireStorage } from 'angularfire2/storage';
+import { AuthGuardService } from "../auth-guard.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  providers: [ AuthenticateService ],
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
@@ -78,7 +78,6 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.storageRef = this.storage.storage.ref();
-    this._service.checkCredentials();
     this.loadPhotosNames();
   }
 
